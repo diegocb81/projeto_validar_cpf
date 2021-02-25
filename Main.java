4dd24492f057934db0bbee0995d7ffcb028eff98
diegocb81 <diegocb81@gmail.com>
2021-02-25 20:28:34 -0300

    Calculando os digitos finais

--- Main.java
+++ Main.java
@@ -4,7 +4,10 @@ public class Main {
 
         int[] numeros;
         numeros = converteNumeros(args);
-        System.out.println(numeros[0]);
+
+        //System.out.println(numeros[0]);
+
+        calculaDigitos(numeros);
 
     }
 
@@ -21,5 +24,50 @@ public class Main {
 
         return numerosInt;
     }
+
+    public static int[] calculaDigitos(int[] numerosCPF){
+
+        int mult = 10;
+        int soma = 0;
+        int dig = 0;
+
+        for (int i=0; i<9; i++){
+            soma = soma+(numerosCPF[i] * mult);
+            mult--;
+        }
+        System.out.println(soma);
+
+        int resto = soma%11;
+
+        if (resto<2){
+            numerosCPF[9] = 0;
+        }else {
+            numerosCPF[9] = 11 - resto;
+        }
+
+        mult = 11;
+        soma = 0;
+
+        for (int i=0; i<10; i++){
+            soma = soma+(numerosCPF[i] * mult);
+            mult--;
+        }
+
+        resto = soma%11;
+
+        if (resto<2){
+            numerosCPF[10] = 0;
+        }else {
+            numerosCPF[10] = 11 - resto;
+        }
+
+        System.out.println(numerosCPF[9]);
+        System.out.println(numerosCPF[10]);
+
+        return numerosCPF;
+
+    }
+
+
 
 }
