0d719999daf284a8b9509001cc15597084dadc73
diegocb81 <diegocb81@gmail.com>
2021-02-26 17:35:04 -0300

    Imprimindo números na formatação do CPF

--- Main.java
+++ Main.java
@@ -5,17 +5,17 @@ public class Main {
         int[] numeros;
         numeros = converteNumeros(args);
 
-        //System.out.println(numeros[0]);
-
-        calculaDigitos(numeros);
+        numeros = calculaDigitos(numeros);
+        imprimeCPF(numeros);
 
     }
 
-    public static int[] converteNumeros(String[] numerosCPF){
+
+    public static int[] converteNumeros(String[] numerosCPF) {
         int[] numerosInt;
         numerosInt = new int[11];
 
-        for (int i=0; i<numerosCPF[0].length(); i++){
+        for (int i = 0; i < numerosCPF[0].length(); i++) {
             char letraVez = numerosCPF[0].charAt(i);
             String str = String.valueOf(letraVez);
             int digitoNum = Integer.parseInt(str);
@@ -25,49 +25,57 @@ public class Main {
         return numerosInt;
     }
 
-    public static int[] calculaDigitos(int[] numerosCPF){
+    public static int[] calculaDigitos(int[] numerosCPF) {
 
         int mult = 10;
         int soma = 0;
         int dig = 0;
 
-        for (int i=0; i<9; i++){
-            soma = soma+(numerosCPF[i] * mult);
+        for (int i = 0; i < 9; i++) {
+            soma = soma + (numerosCPF[i] * mult);
             mult--;
         }
-        System.out.println(soma);
 
-        int resto = soma%11;
+        int resto = soma % 11;
 
-        if (resto<2){
+        if (resto < 2) {
             numerosCPF[9] = 0;
-        }else {
+        } else {
             numerosCPF[9] = 11 - resto;
         }
 
         mult = 11;
         soma = 0;
 
-        for (int i=0; i<10; i++){
-            soma = soma+(numerosCPF[i] * mult);
+        for (int i = 0; i < 10; i++) {
+            soma = soma + (numerosCPF[i] * mult);
             mult--;
         }
 
-        resto = soma%11;
+        resto = soma % 11;
 
-        if (resto<2){
+        if (resto < 2) {
             numerosCPF[10] = 0;
-        }else {
+        } else {
             numerosCPF[10] = 11 - resto;
         }
-
-        System.out.println(numerosCPF[9]);
-        System.out.println(numerosCPF[10]);
 
         return numerosCPF;
 
     }
 
+    public static void imprimeCPF(int[] numerosCPF){
 
+        for (int i=0; i<numerosCPF.length; i++){
+            if ((i%3==0)&&(i>0)&&(i<9)){
+                System.out.print(".");
+            }
+            if (i==9){
+                System.out.print("-");
+            }
+            System.out.print(numerosCPF[i]);
+        }
+
+    }
 
 }
